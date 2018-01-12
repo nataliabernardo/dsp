6cd751a47b905adfb5363d5c73d0203e3896c1b2
nataliabernardo <31417244+nataliabernardo@users.noreply.github.com>
2018-01-12 15:17:13 -0800

    Update q5_datetime.py

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -2,12 +2,39 @@
 
 ####a) 
 date_start = '01-02-2013'  
-date_stop = '07-28-2015'   
+date_stop = '07-28-2015' 
+
+from datetime import datetime
+
+def days_between(d1, d2):
+    d1 = datetime.strptime(d1, "%m-%d-%Y")
+    d2 = datetime.strptime(d2, "%m-%d-%Y")
+    return abs((d2 - d1).days)
+
+days_between(date_start, date_stop)
 
 ####b)  
 date_start = '12312013'  
 date_stop = '05282015'  
 
+from datetime import datetime
+
+def days_between(d1, d2):
+    d1 = datetime.strptime(d1, "%m%d%Y")
+    d2 = datetime.strptime(d2, "%m%d%Y")
+    return abs((d2 - d1).days)
+
+days_between(date_start, date_stop)
+
 ####c)  
 date_start = '15-Jan-1994'  
-date_stop = '14-Jul-2015'  
+date_stop = '14-Jul-2015' 
+
+from datetime import datetime
+
+def days_between(d1, d2):
+    d1 = datetime.strptime(d1, "%d-%b-%Y")
+    d2 = datetime.strptime(d2, "%d-%b-%Y")
+    return abs((d2 - d1).days)
+
+days_between(date_start, date_stop)
